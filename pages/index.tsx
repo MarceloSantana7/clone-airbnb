5556293be640d8a77f9411382c37eba84eb9849d
Marcelo Santana <marcelo.ssantos127@gmail.com>
2023-02-19 01:22:49 -0300

    Finished MediumCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from './../components/Header';
 import Banner from './../components/Banner';
+import SmallCard from '../components/SmallCard';
+import MediumCard from './../components/MediumCard';
 
-const Home: NextPage = () => {
+type ExploreData = {
+  location: string;
+  distance: string;
+  img: string;
+};
+
+type CardsData = {
+  img: string;
+  title: string;
+};
+
+
+type HomeProps = {
+  exploreData: ExploreData[];
+  cardsData: CardsData[];
+};
+
+
+const Home: NextPage<HomeProps> = ({exploreData, cardsData}) => {
   return (
     <div className="">
       <Head>
@@ -13,8 +32,50 @@ const Home: NextPage = () => {
       </Head>
       <Header/>
       <Banner/>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+          {exploreData?.map(({img, distance, location}) => (
+            <SmallCard 
+            key={img}
+            img={img} 
+            distance={distance} 
+            location={location}/>
+          ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+          {cardsData?.map(({img, title}) => (
+            <MediumCard key={img} img={img} title={title}/>
+          ))}
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
 
 export default Home
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+    (res) => res.json()
+  )
+
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      exploreData: exploreData,
+      cardsData: cardsData
+    }
+  }
+}
