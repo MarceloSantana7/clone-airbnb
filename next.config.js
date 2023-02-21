854698fa152e045678ac44fa2c55964bb700ef2d
Marcelo Santana <marcelo.ssantos127@gmail.com>
2023-02-21 15:35:17 -0300

    Fix map

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     domains: ["links.papareact.com"]
   },
   env: {
-    mapbox_key: "pk.eyJ1IjoibWFyY2Vsb3NhbnRhbmE3IiwiYSI6ImNsZWNxdWQwajAwZnA0M3A3c2RnbjM2aG0ifQ.FWKE7o50uOuSu6n5OqZCOw"
+    mapbox_key: "pk.eyJ1IjoibWFyY2Vsb3NhbnRhbmE3IiwiYSI6ImNsZWVkOTZqeTAwMnYzcW9rZmtwcGU4dDEifQ.TWCR5jV25GeSLDfXB9M_sA"
   },
   reactStrictMode: true,
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,49 +25,48 @@ type HomeProps = {
 };
 
 
-const Home: NextPage<HomeProps> = ({exploreData, cardsData}) => {
+const Home: NextPage<HomeProps> = ({ exploreData, cardsData }) => {
   return (
     <div className="">
       <Head>
         <title>AirBnb - Página Inicial</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header placeholder={""}/>
-      <Banner/>
+      <Header placeholder={""} />
+      <Banner />
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          
+
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-          {exploreData?.map(({img, distance, location}) => (
-            <SmallCard 
-            key={img}
-            img={img} 
-            distance={distance} 
-            location={location}/>
-          ))}
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location} />
+            ))}
           </div>
         </section>
 
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-          
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
-          {cardsData?.map(({img, title}) => (
-            <MediumCard key={img} img={img} title={title}/>
-          ))}
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
           </div>
 
           <LargeCard
-          img='https://links.papareact.com/4cj'
-          title="The Greatest Outdoors"
-          description='Wishlists curated by Airbnb'
-          buttonText='Get Inspired'
+            img='https://links.papareact.com/4cj'
+            title="The Greatest Outdoors"
+            description='Wishlists curated by Airbnb'
+            buttonText='Get Inspired'
           />
-          
+
         </section>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
